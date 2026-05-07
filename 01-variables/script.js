1477aa5d759f92b239498138d3b40f6f6ed61383
Kara Aishwarya <aishwaryaa1902k@gmail.com>
2026-05-08 00:26:21 +0530

    Initial commit

---
+++ 01-variables/script.js
@@ -0,0 +1,24 @@
+// VARIABLES
+
+// variable is used to store the data
+
+// STRING = text
+
+let firstname = 'Aishwarya';
+
+// NUMBER 
+let age = 23;
+
+// BOOLEAN = true or false
+let student = true;
+
+// Displaying the above data in the console
+
+console.log(firstname);
+console.log(age);
+console.log(student);
+
+// Displaying the data on a webpage
+document.getElementById('p1').textContent = `Name : ${firstname}`;
+document.getElementById('p2').textContent = `Age : ${age}`;
+document.getElementById('p3').textContent = `Student : ${student}`;
---
+++ website/index.js
@@ -0,0 +1,16 @@
+//console.log('hello');
+//console.log('i like pink');
+
+//window.alert('this is an alert!');
+//window.alert('i like pink');
+
+document.getElementById('my-h1')
+
+// this is a comment
+/* 
+    this 
+    is 
+    a
+    comment
+
+*/
